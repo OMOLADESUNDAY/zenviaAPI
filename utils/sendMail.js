18aec7e6500683f84402c6d7cec363628818eeb1
spyer <sundayomoladee11@gmail.com>
2025-12-10 12:28:47 +0100

    RESEND EMAIL

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -2,26 +2,47 @@ import nodemailer from "nodemailer";
 import dotenv from "dotenv";
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // use TLS
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_APP_PASSWORD,
-  },
-});
+// const transporter = nodemailer.createTransport({
+//   host: "smtp.gmail.com",
+//   port: 587,
+//   secure: false, // use TLS
+//   auth: {
+//     user: process.env.GMAIL_USER,
+//     pass: process.env.GMAIL_APP_PASSWORD,
+//   },
+// });
+
+// export const sendEmail = async ({ to, subject, html }) => {
+//   try {
+//     await transporter.sendMail({
+//       from: process.env.GMAIL_USER,
+//       to,
+//       subject,
+//       html,
+//     });
+//   } catch (err) {
+//     console.error("Email sending failed:", err);
+//     throw new Error("Failed to send email");
+//   }
+// };
+
+
+import { Resend } from "resend";
+import dotenv from "dotenv";
+dotenv.config();
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-    await transporter.sendMail({
-      from: process.env.GMAIL_USER,
+    await resend.emails.send({
+      from: "Verify <onboarding@resend.dev>",  // MUST be resend.dev
       to,
       subject,
       html,
     });
   } catch (err) {
-    console.error("Email sending failed:", err);
+    console.error("Resend Email Error:", err);
     throw new Error("Failed to send email");
   }
 };
