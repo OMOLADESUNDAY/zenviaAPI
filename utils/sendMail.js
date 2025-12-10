8a488a6078cb4bd73e973ba67e3dbbdfe9273e8c
spyer <sundayomoladee11@gmail.com>
2025-12-10 12:31:31 +0100

    RESEND EMAIL

--- utils/sendMail.js
+++ utils/sendMail.js
@@ -1,6 +1,6 @@
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
-dotenv.config();
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
+// dotenv.config();
 
 // const transporter = nodemailer.createTransport({
 //   host: "smtp.gmail.com",
