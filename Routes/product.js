0bc795c40b307c21b8c00fc732c19d32b5a79365
spyer <sundayomoladee11@gmail.com>
2025-12-10 22:11:14 +0100

    get all product

--- Routes/product.js
+++ Routes/product.js
@@ -1,5 +1,6 @@
 import express from 'express'
+import { getProducts,getSingleProduct } from '../controllers/product'
 const router=express.Router()
 router.get('/:id',getSingleProduct)
-router.get('/',getAllProduct)
+router.get('/',getProducts)
 export default router
